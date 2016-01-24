f72e9152e6d6731281323627b721aed6fb792b8b
2tom <tera1215@gmail.com>
2016-01-24 21:18:37 +0900

    modified Header

--- src/js/components/_Header.jsx
+++ src/js/components/_Header.jsx
@@ -3,31 +3,30 @@ import {AppBar, LeftNav, MenuItem} from 'material-ui';
 const ThemeManager = require('material-ui/lib/styles/theme-manager');
 import MyRawTheme from '../mui_theme.jsx';
 
-class Header extends React.Component {
 
-  handleToggle = () => this.setState({open: !this.state.open});
-  handleClose = () => this.setState({open: false});
+class Header extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {open: false};
-  },
+  }
+
+  handleToggle = () => this.setState({open: !this.state.open});
+
+  handleClose = () => this.setState({open: false});
 
   render() {
     return (
       <div>
-        <AppBar
-          title="CIoT"
-          iconClassNameRight="muidocs-icon-navigation-expand-more"
-          onTouchTap={this.handleToggle}
-        />
-
+        <RaisedButton
+          label="Undocked Controlled LeftNav With Custom Width"
+          onTouchTap={this.handleToggle} />
         <LeftNav
           docked={false}
           width={200}
           open={this.state.open}
           onRequestChange={open => this.setState({open})}
-          >
+        >
           <MenuItem onTouchTap={this.handleClose}>Menu Item</MenuItem>
           <MenuItem onTouchTap={this.handleClose}>Menu Item 2</MenuItem>
         </LeftNav>
